57d4c1b78ef6c575f3cd040e80b9a1e63309d905
NatanaelAlvin <91649861+NatanaelAlvin@users.noreply.github.com>
2026-04-30 23:11:12 +0700

    Refactor CategoryFilter button styles and layout
    Updated button styles and layout in CategoryFilter component.

--- Frontend/src/components/categoryFilter.tsx
+++ Frontend/src/components/categoryFilter.tsx
@@ -8,21 +8,20 @@ interface Props {
 
 export default function CategoryFilter({ categories, selected, setSelected }: Props) {
   return (
-    <div>
-      <button onClick={() => setSelected(null)}>Semua</button>
+    <div style={{ display: "flex", gap: 10, marginBottom: 20 }}>
+  <button onClick={() => setSelected(null)}>Semua</button>
 
-      {categories.map(c => (
-        <button
-          key={c.id}
-          onClick={() => setSelected(c.id)}
-          style={{
-            margin: 5,
-            backgroundColor: selected === c.id ? "yellow" : "white"
-          }}
-        >
-          {c.name}
-        </button>
-      ))}
-    </div>
-  );
+  {categories.map(c => (
+    <button
+      key={c.id}
+      onClick={() => setSelected(c.id)}
+      style={{
+        background: selected === c.id ? "#9d9d9c" : "#eee"
+      }}
+    >
+      {c.name}
+    </button>
+  ))}
+</div>
+  ); 
 }
